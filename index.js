d113eba804d185314b2334ec86e452f99d20d77b
Javier Marugán <marugandev@gmail.com>
2025-04-03 12:51:39 +0200

    Feat/rr 0005 (#10)
    * feat(RR-0001): New architecture approach (#1)
    
    * feat/RR-0002 docs: add README with project overview and Git workflow (#4)
    
    * Feat/rr 0003 (#6) (#7)
    
    * feat(RR-0001): New architecture approach (#1)
    
    * feat/RR-0002 docs: add README with project overview and Git workflow (#4)
    
    * feat: error middleware
    
    ---------
    
    Co-authored-by: SantiagoCabreraEscoriza <163005370+SantiagoCabreraEscoriza@users.noreply.github.com>
    Co-authored-by: Jesus Elias Alba <61273344+jesuseliasalba@users.noreply.github.com>
    
    * feat: Register log function (#8) (#9)
    
    * feat(RR-0005): function connect database
    
    ---------
    
    Co-authored-by: SantiagoCabreraEscoriza <163005370+SantiagoCabreraEscoriza@users.noreply.github.com>
    Co-authored-by: Jesus Elias Alba <61273344+jesuseliasalba@users.noreply.github.com>
    Co-authored-by: Manu Chaso <47826459+ManuChaso@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const {errorMiddleware} = require('./src/middlewares/error_middleware');
-const logger = require('./src/logs/logs');
+
+require("dotenv").config();
+const express = require("express");
+const { errorMiddleware } = require("./src/middlewares/error_middleware");
+const logger = require("./src/logs/logs");
+const connectDb = require("./src/config/db/db");
+
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+connectDb();
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
 app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
-})
+  logger.info(`Server running on port ${PORT}`);
+});
+
+
+
