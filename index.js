7119dbe02307ca1b8fcac412f23306d289806c98
Manu Chaso <47826459+ManuChaso@users.noreply.github.com>
2025-04-03 12:21:16 +0200

    feat: Register log function (#8)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const {errorMiddleware} = require('./src/middlewares/error_middleware');
+const logger = require('./src/logs/logs');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,5 +9,5 @@ const app = express();
 app.use(errorMiddleware)
 
 app.listen(PORT, () => {
-    console.log("Server running on port: ", PORT)
+    logger.info(`Server running on port ${PORT}`)
 })
