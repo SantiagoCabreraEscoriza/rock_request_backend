9d565a8c9c6b403761a1e87a865d28550680f992
Manu Chaso <47826459+ManuChaso@users.noreply.github.com>
2025-04-03 11:42:55 +0200

    Feat/rr 0003 (#6)
    * feat(RR-0001): New architecture approach (#1)
    
    * feat/RR-0002 docs: add README with project overview and Git workflow (#4)
    
    * feat: error middleware
    
    ---------
    
    Co-authored-by: SantiagoCabreraEscoriza <163005370+SantiagoCabreraEscoriza@users.noreply.github.com>
    Co-authored-by: Jesus Elias Alba <61273344+jesuseliasalba@users.noreply.github.com>

---
+++ src/middlewares/error_middleware.js
@@ -0,0 +1,26 @@
+class ApiError extends Error{
+    /**
+     * 
+     * @param {string} message 
+     * @param {number} statusCode 
+     */
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode = statusCode;
+    }
+}
+
+function errorMiddleware(err, req, res, next){
+    console.log(err.stack);
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).send({
+        error: {
+            message: err.message,
+            statusCode: err.statusCode
+        }
+    })
+}
+
+module.exports = { errorMiddleware, ApiError }
